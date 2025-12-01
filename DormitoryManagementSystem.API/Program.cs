bdca9094f69b5086b9f217a12f03f47cc19f3c81
NgHuuDu <huuduit1907@gmail.com>
2025-12-01 15:29:26 +0700

    Update Program.cs
    cho biết có sử dụng JWT để test Swagger UI

--- DormitoryManagementSystem.API/Program.cs
+++ DormitoryManagementSystem.API/Program.cs
@@ -6,6 +6,7 @@ using DormitoryManagementSystem.DAO.Implementations;
 using DormitoryManagementSystem.DAO.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,43 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DormitoryManagementSystem.API", Version = "v1" });
+
+    // --- PHẦN BỔ SUNG ĐỂ HIỆN NÚT AUTHORIZE ---
+    
+    // Định nghĩa loại bảo mật là "Bearer" (JWT)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    // Yêu cầu Swagger sử dụng định nghĩa trên cho các API
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 // CORS
 builder.Services.AddCors(options =>
